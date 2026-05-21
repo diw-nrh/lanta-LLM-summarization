31b4196ad315a93f74823c73cd56718e6a88c4e6
Kakuren <klong8257@gmail.com>
2026-05-22 00:55:03 +0700

    Add initial implementations for AgentARetriever and FormatterNode classes

--- nodes/agent_a_retriever.py
+++ nodes/agent_a_retriever.py
@@ -2,7 +2,7 @@ from state import main_state
 import numpy as np
 class AgentARetriever:
     def __init__(self):
-        
+        pass
     def retrieve_node(state: main_state):
         #ใช้ query หา paras ที่เกี่ยวข้อง
         return 0
