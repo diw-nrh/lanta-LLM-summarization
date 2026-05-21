b7c61e8b624dffcbc2ada38e92afd4e47c4ade78
Kakuren <klong8257@gmail.com>
2026-05-22 00:25:29 +0700

    Refactor agent classes to encapsulate functionality within class structures

--- nodes/agent_a_retriever.py
+++ nodes/agent_a_retriever.py
@@ -1,4 +1,8 @@
 from state import main_state
-def retrieve_node(state: main_state):
-    #ใช้ query หา paras ที่เกี่ยวข้อง
-    return 0
+import numpy as np
+class AgentARetriever:
+    def __init__(self):
+        
+    def retrieve_node(state: main_state):
+        #ใช้ query หา paras ที่เกี่ยวข้อง
+        return 0
